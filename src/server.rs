75f994e596788519b44ed03c6436cbb62e2a338a
Markus Kohlhase <mail@markus-kohlhase.de>
2020-08-17 23:39:53 +0200

    Update dependencies (#10)
    Co-authored-by: Markus Kohlhase <markus.kohlhase@slowtec.de>

--- src/server.rs
+++ src/server.rs
@@ -20,13 +20,11 @@ pub async fn start(port: u16, path: String, is_spa: bool, spa_index: &str) {
 
     let files = warp::fs::dir(path);
     let spa = warp::any()
-        .and_then(move || {
-            async move {
-                if is_spa {
-                    Ok(is_spa)
-                } else {
-                    Err(warp::reject::not_found())
-                }
+        .and_then(move || async move {
+            if is_spa {
+                Ok(is_spa)
+            } else {
+                Err(warp::reject::not_found())
             }
         })
         .and(warp::fs::file(spa_index_path))
